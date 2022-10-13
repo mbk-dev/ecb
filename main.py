90185b09a667294b4ba32d8ba7a31ada1a6d6f0b
fzhulitov <fzhulitov@gmail.com>
2022-10-13 17:01:30 +0300

    All work but main rate has 5000+ pos overs has 8000+

--- main.py
+++ main.py
@@ -18,5 +18,11 @@ if __name__ == '__main__':
 import pandas as pd
 import ecb
 
-abc = ecb.kr.get_mrr_fr()
-print("jkshld")
+abc = ecb.kr.get_main_rate()
+print(abc)
+
+abc = ecb.kr.get_deposit_rate()
+print(abc)
+
+abc = ecb.kr.get_marginal_rate()
+print(abc)
