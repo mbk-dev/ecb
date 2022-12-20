58f5fa1b36d3dbfedc6f9c12b1ea734ec8320349
Sergey Kikevich <sergey@rostsber.ru>
2022-12-20 08:28:32 +0300

    chore: update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import ecb
 
 abc = ecb.kr.get_refinancing_rate()
-print(abc)
+print(abc.tail(5))
 
 abc = ecb.kr.get_deposit_rate()
-print(abc)
+print(abc.tail(5))
 
 abc = ecb.kr.get_marginal_rate()
-print(abc)
+print(abc.tail(5))
 
 abc = ecb.gdp.get_gdp_q()
-print(abc)
+print(abc.tail(5))
 
 abc = ecb.hicp.get_hicp()
-print(abc)
+print(abc.tail(5))
 
 print(ecb.__version__)
