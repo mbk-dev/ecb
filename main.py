44d3504ec5d1e650f05b3be748f97f67edd3734d
fzhulitov <fzhulitov@gmail.com>
2023-01-11 13:14:54 +0200

    Key Rates, parametrs start_date, end_date as pd.Timestamp, all working, korrect return in decimal

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+import pandas as pd
+
 import ecb
 
-abc = ecb.kr.get_refinancing_rate()
+abc = ecb.kr.get_refinancing_rate(start_date=pd.Timestamp(2022, 1, 1))
 print(abc)
 
-abc = ecb.kr.get_deposit_rate()
+abc = ecb.kr.get_deposit_rate(start_date=pd.Timestamp(2022, 1, 1))
 print(abc)
 
-abc = ecb.kr.get_marginal_rate()
+abc = ecb.kr.get_marginal_rate(start_date=pd.Timestamp(2022, 1, 1))
 print(abc)
 
-abc = ecb.gdp.get_gdp_q()
-print(abc)
+#abc = ecb.gdp.get_gdp_q()
+#print(abc)
 
-abc = ecb.hicp.get_hicp()
-print(abc)
+#abc = ecb.hicp.get_hicp()
+#print(abc)
 
 print(ecb.__version__)
