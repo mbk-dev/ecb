637fcaa2bc4dd7b4819b91f3cfb31ac6b26a1f85
Sergey Kikevich <sergey@rostsber.ru>
2024-03-07 14:44:56 +0300

    chore: update main.py

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import pandas as pd
 
 import ecb
 
-
-
 print(ecb.__version__)
+
+abc = ecb.kr.get_refinancing_rate()
+print(abc.tail(5))
+
+abc = ecb.kr.get_deposit_rate()
+print(abc.tail(5))
+
+abc = ecb.kr.get_marginal_rate()
+print(abc.tail(5))
+
+abc = ecb.gdp.get_gdp_q()
+print(abc.tail(5))
+
+abc = ecb.hicp.get_hicp()
+print(abc.tail(5))
